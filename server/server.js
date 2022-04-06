3bf907bbc0eaa2f8dd04abd7deea15e822ebf598
MaxAndreasLundin <lundin-89@hotmail.com>
2022-04-06 10:31:26 +0200

    Express/Node base

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree", "userFour"] });
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
