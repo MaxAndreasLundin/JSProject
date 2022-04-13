c9678a962b8666fdff0bd4775f5f57114d88f7b6
Koarito <amiromarhonein@hotmail.fr>
2022-04-13 20:14:58 +0200

    Removed some stuff

--- client/src/ActivityCard.js
+++ client/src/ActivityCard.js
@@ -2,21 +2,17 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { memo } from "react";
 import Button from "@mui/material/Button";
-import SimpleContainer from "./SimpleContainer";
+import { flexbox } from '@mui/system';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -37,38 +33,27 @@ function RecipeReviewCard(props) {
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 500}}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
+          <Avatar src='https://wl-brightside.cf.tsp.li/resize/728x/jpg/881/409/62d49f51afa2c42e7b0d0e37e6.jpg'/>
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={props.title}
-        subheader="Hökarängen, 29 Mars 2022"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://images.unsplash.com/photo-1571302703929-6209091a75fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
-        alt="Korv"
-      />
-      <CardContent>
-        <SimpleContainer>
-          <Typography variant="body2" color="text.secondary">
-            Korvgrillning i skogen. Joina!!!
-          </Typography>
-        </SimpleContainer>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
+          <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
+        }
+        title = {props.title}
+        subheader="Hökarängen, 29 Mars 2022"
+      />
+    
+      <CardContent>
+          <Typography>
+           Ålder:
+           <Button sx={{ alignContent: 'flex-end'}} variant="contained">Alla åldrar</Button>
+          </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
 
         <ExpandMore
           expand={expanded}
