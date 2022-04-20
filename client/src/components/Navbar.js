6f42d8662c02ab949ab90d0433b1b32bf523532a
Koarito <amiromarhonein@hotmail.fr>
2022-04-20 19:45:12 +0200

    changed to basic bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,49 +2,16 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import ChatIcon from "@mui/icons-material/Chat";
-import { flexbox } from "@mui/system";
 
-export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? "Logout" : "Login"}
-        />
-      </FormGroup>
       <AppBar position="static" color="info">
-        <Toolbar sx={{ display: "flex" }}>
+        <Toolbar>
           <IconButton
             size="large"
             edge="start"
@@ -54,46 +21,10 @@ export default function MenuAppBar() {
           >
             <MenuIcon />
           </IconButton>
-
-          <IconButton size="large" sx={{ alignItems: "center" }}>
-            <img src={"/Vecktor.svg"} />
-          </IconButton>
-
-          {auth && (
-            <div>
-              <IconButton size="large">
-                <ChatIcon />
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <PersonOutlineIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
