f01c3fc3c15bd9aa2ef6430c556771821578e7c0
Koarito <amiromarhonein@hotmail.fr>
2022-04-20 19:49:03 +0200

    added icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,10 +2,10 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import MessageIcon from "@mui/icons-material/Message";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 export default function ButtonAppBar() {
   return (
@@ -21,10 +21,12 @@ export default function ButtonAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton size="large">
+            <MessageIcon />
+          </IconButton>
+          <IconButton size="large">
+            <PersonOutlineIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
