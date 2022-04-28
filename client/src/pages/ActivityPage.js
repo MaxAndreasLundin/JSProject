7f519864adb1367f6580222b77a2abd4256d8bf1
MaxAndreasLundin <lundin-89@hotmail.com>
2022-04-28 11:10:39 +0200

    fixed activitpage intro

--- client/src/pages/ActivityPage.js
+++ client/src/pages/ActivityPage.js
@@ -2,50 +2,35 @@ import React from "react";
 import { Box } from "@mui/system";
 import { Container, Typography } from "@mui/material";
 import Background from "../assets/kids2Filter.png";
-import Pink from "../assets/pink.png";
 import ActivityCard from "../components/UpdatedActivityCard";
 
 function ActivityPage() {
   return (
     <React.Fragment>
       <Container
+        variant="flex"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
           backgroundImage: `url(${Background})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
-          p: 0,
+          alignItems: "flex-start",
         }}
       >
-        <Box>
+        <Box sx={{ mb: 6, ml: 1, mr: 1 }}>
           <Typography
             variant="h1"
-            align="left"
             sx={{
-              ml: -14,
-              mb: 2,
-              mt: 3,
               color: "#FFFFFF",
               fontSize: "16px",
             }}
           >
             <h1>Joina en aktivitet!</h1>
           </Typography>
-        </Box>
-        <Box>
           <Typography
             variant="subtitle1"
-            align="left"
             sx={{
-              ml: 3,
-              mr: 3,
-              mb: 2,
               color: "#FFFFFF",
               fontSize: "14px",
-              fontWeight: "regular",
             }}
           >
             Här nedan kan du se vilka aktiviteter som våra fina medlemmar eller
@@ -57,15 +42,9 @@ function ActivityPage() {
         </Box>
       </Container>
       <Container
+        variant="flex"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundImage: `url(${Pink})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          p: 0,
+          backgroundColor: "#FBEEED",
         }}
       >
         <ActivityCard />
