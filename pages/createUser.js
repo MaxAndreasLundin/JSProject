1cbea8cc5de05b0e2128aa808bdb5ce85f6b6e69
Koarito <amiromarhonein@hotmail.fr>
2022-05-02 00:50:06 +0200

    have some validation and components to work on

--- pages/createUser.js
+++ pages/createUser.js
@@ -1,15 +1,12 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
-import { Typography, Box, Container, FormGroup } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
+import { Typography, Box, FormControlLabel } from "@mui/material";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
@@ -39,38 +36,67 @@ export default function CreateUser() {
 
   return (
     <form>
-      <Container variant="flex" maxWidth="90%">
-        <Box>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        maxWidth="90%"
+        margin="auto"
+        spacing={2}
+      >
+        <Grid>
           <Typography variant="signupHeader">Skapa Konto</Typography>
-        </Box>
+        </Grid>
 
-        <FormControl fullWidth>
-          <InputLabel color="info">Lösenord*</InputLabel>
-          <OutlinedInput
-            fullWidth
-            required
-            color="info"
-            label="Lösenord"
-            type={values.showPassword ? "text" : "password"}
-            value={values.password}
-            onChange={handleChange("password")}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
+        <Grid item sx={{ width: 1 }}>
+          <FormControl fullWidth>
+            <InputLabel color="info">För- och efternamn</InputLabel>
+            <OutlinedInput
+              color="info"
+              label="För- och efternamn"
+              value={values.name}
+              onChange={handleChange("name")}
+            />
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel color="info">För- och efternamn</InputLabel>
+            <OutlinedInput
+              color="info"
+              label="För- och efternamn"
+              value={values.name}
+              onChange={handleChange("name")}
+            />
+          </FormControl>
+        </Grid>
+
+        <Grid item sx={{ width: 1 }}>
+          <FormControl fullWidth>
+            <InputLabel color="info">Lösenord*</InputLabel>
+            <OutlinedInput
+              required
+              color="info"
+              label="Lösenord"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
           <FormControl fullWidth>
             <InputLabel color="info">Upprepa lösenord*</InputLabel>
             <OutlinedInput
-              fullWidth
               color="info"
               label="Upprepa lösenord"
               type={values.showPassword ? "text" : "password"}
@@ -90,8 +116,31 @@ export default function CreateUser() {
               }
             />
           </FormControl>
-        </FormControl>
-      </Container>
+          <Typography variant="pswrdInfo">
+            Använd minst åtta tecken och en kombination av bokstäver, siffror
+            och symboler
+          </Typography>
+        </Grid>
+        <Grid item alignItems="flex-start">
+          <FormControlLabel
+            control={<Checkbox color="info" />}
+            label={
+              <Typography variant="pswrdInfo">
+                Jag tillåter att Join skickar påminnelser om aktiviteter och
+                liknande till min mail
+              </Typography>
+            }
+          />
+          <FormControlLabel
+            control={<Checkbox color="info" />}
+            label={
+              <Typography variant="pswrdInfo">
+                Jag godkänner Joins användarvillkor
+              </Typography>
+            }
+          />
+        </Grid>
+      </Grid>
     </form>
   );
 }
