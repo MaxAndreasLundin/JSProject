390fa8710b836895e10351f2af059ab1c1f6fb01
MaxAndreasLundin <lundin-89@hotmail.com>
2022-05-04 19:08:09 +0200

    crud working

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI:
+      "mongodb+srv://Max:maxlundin@cluster0.cctkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
 };
