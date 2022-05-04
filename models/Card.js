7630b2405633db3c2dc47ad9b8ec2c7f0f11151a
MaxAndreasLundin <lundin-89@hotmail.com>
2022-05-04 21:26:52 +0200

    added fields to db

--- models/Card.js
+++ models/Card.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const CardSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "Please adda title"],
+    required: [true, "Please add a title"],
     unique: true,
     trim: true,
     maxlength: [40, "Title max amount of characters is 40"],
@@ -12,6 +12,21 @@ const CardSchema = new mongoose.Schema({
     type: String,
     required: true,
     maxlength: [200, "Description max amount of characters is 200"],
+  },
+  place: {
+    type: String,
+    required: true,
+    maxlength: [40, "Max amount of characters is 40"],
+  },
+  date: {
+    type: String,
+    required: true,
+    maxlength: [40, "Max amount of characters is 40"],
+  },
+  time: {
+    type: String,
+    required: true,
+    maxlength: [6, "Max amount of characters is 6"],
   },
 });
 
