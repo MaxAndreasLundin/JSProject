d5863d9b1418e333f7ea0d66166d863ea830ef8a
Max <lundin-89@hotmail.com>
2022-05-05 10:30:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     MONGO_URI:
-      "mongodb+srv://Max:maxlundin@cluster0.cctkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "",
   },
 };
