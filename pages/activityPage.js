c3902837d834c9c63244f2f118ab9dec7926f808
MaxAndreasLundin <lundin-89@hotmail.com>
2022-05-13 22:02:48 +0200

    revalidate 1

--- pages/activityPage.js
+++ pages/activityPage.js
@@ -100,7 +100,7 @@ export async function getStaticProps() {
     return card;
   });
 
-  return { props: { cards: cards } };
+  return { props: { cards: cards }, revalidate: 1 };
 }
 
 export default ActivityPage;
