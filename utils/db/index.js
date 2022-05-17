4272900956f6ca6d383b27ec4ca020125cbc7637
Marion Michielsen <91435399+MarionMichielsen@users.noreply.github.com>
2022-05-17 14:09:26 +0200

    "tog bort kommatecken .env nu funkar det!"

--- utils/db/index.js
+++ utils/db/index.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import "dotenv/config";
 
 const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
